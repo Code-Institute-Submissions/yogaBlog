ad2362203d753a3d1b32d6e02cf03c8ba46b2edd
Wayne Pegg <peggy161152@gmail.com>
2020-03-18 16:43:03 +0000

    rollback due to data issues

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://Blogger:Blogger@yogacluster-kiw6r.mongodb.net/yogaBlog?retryWrites=true&w=majority"
@@ -8,9 +9,12 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Wayne'}
     return render_template('index.html', title="Home", user=user, posts=mongo.db.blogEntries.find())
+
+    
+
+
 
 
